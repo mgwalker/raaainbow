51e31d14eaf15d146b7b1d8ad33d3c8899efd03a
mgwalker <mgwalker@gmail.com>
2015-08-29 15:28:49 -0500

    Use cli-position

--- src/modes/circle.js
+++ src/modes/circle.js
@@ -1,4 +1,5 @@
 const colors = require("../colors");
+const pos = require("cli-position");
 
 module.exports = {
 	name: "circle",
@@ -11,25 +12,17 @@ module.exports = {
 			if(angle > 2 * Math.PI) {
 				angle = 0;
 			}
-			let centerX = Math.round(process.stdout.columns / 2);
-			let centerY = Math.round(process.stdout.rows / 2);
+			let centerX = Math.round(pos.columns() / 2);
+			let centerY = Math.round(pos.rows() / 2);
 			let radius = (centerX < centerY ? centerX : centerY);
 
 			// Center the cursor
-			process.stdout.write(`\x1b[${centerY};${centerX}f`);
+			pos.center();
 			
 			let xOffset = Math.round(Math.sin(angle) * (radius * 2));	// a single terminal space is about twice as tall as wide, so account for that
 			let yOffset = Math.round(Math.cos(angle) * radius);
-			if(xOffset > 0) {
-				process.stdout.write(`\x1b[${xOffset}C`);
-			} else {
-				process.stdout.write(`\x1b[${xOffset}D`);
-			}
-			if(yOffset > 0) {
-				process.stdout.write(`\x1b[${yOffset}A`);
-			} else {
-				process.stdout.write(`\x1b[${yOffset}B`);
-			}
+			pos.moveLeft(xOffset).moveUp(yOffset);
+
 			process.stdout.write(`${colors[colorIndex % colors.length]}m `);
 			
 			angle += 0.04;

24e2a1f57dae94eea73f3cdd0fadae3cf748c5ea
mgwalker <mgwalker@gmail.com>
2015-08-30 18:55:12 -0500

    Add linear scale and make the sine wave move

--- src/modes/sine.js
+++ src/modes/sine.js
@@ -1,32 +1,30 @@
 const pos = require("cli-position");
 const colors = require("../colors");
-
-function scale(inRange, outRange) {
-	const inDelta = inRange[1] - inRange[0];
-	const outDelta = outRange[1] - outRange[0];
-	
-	return function(val) {
-		return (((val - inRange[0]) / inDelta) * outDelta) + outRange[0];//((val / inDelta) * outDelta) + outRange[0];
-	}
-};
+const scale = require("linear-scale");
 
 module.exports = {
 	name: "sine",
 	run: function() {
 		pos.center();
 		
-		setTimeout(() => {
+		let xOffset = 0;
+		setInterval(() => {
 			const columns = pos.columns();
 			const yTop = pos.rows() / 3;
 			const yCenter = pos.rows() / 2;
 			
 			const xScale = scale([0, columns], [0, 4 * Math.PI]);
 			
+			// Clear the screen, because that's easier than
+			// erasing the previous line
+			console.log('`\x1b[m\x1b[2J');
+			
 			for(let x = 0; x < columns; x++) {
-				let y = Math.round((Math.sin(xScale(x)) * yTop) + yCenter);
+				let y = Math.round((Math.sin(xScale(x + xOffset)) * yTop) + yCenter);
 				pos.moveTo(x, y);
 				process.stdout.write(`${colors[x % colors.length]} `);
 			}
-		}, 100);
+			xOffset++;
+		}, 20);
 	}
 };
